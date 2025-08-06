38fc5f06c4ecb629fc7b57b39858d311f4f1f256
Tinapen <cristinapenaflor20@gmail.com>
2025-08-07 02:39:56 +0800

    Logo updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Logo from '../assets/logo.svg';
 
 const Footer = () => {
   return (
@@ -9,7 +10,7 @@ const Footer = () => {
     <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
         <div className="sm:flex sm:items-center sm:justify-between">
             <a href="https://flowbite.com/" className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
-                <img src="/assets/logo.svg" className="h-8" alt="logo" />
+                <img src={Logo} className="h-8" alt="logo" />
                 <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">My Site</span>
             </a>
             <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Logo from '../assets/logo.svg';
 
 const Header = () => {
   return (
@@ -8,7 +9,7 @@ const Header = () => {
 <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
   <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
   <a href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
-      <img src="/assets/logo.svg" className="h-8" alt="logo" />
+      <img src={Logo} className="h-8" alt="logo" />
       <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">My Site</span>
   </a>
   <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">

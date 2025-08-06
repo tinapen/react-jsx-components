ee20f9244a00b5975e2aa3c92d4a252a93fb1b1d
Tinapen <cristinapenaflor20@gmail.com>
2025-08-07 02:33:34 +0800

    updated logo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
     <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
         <div className="sm:flex sm:items-center sm:justify-between">
             <a href="https://flowbite.com/" className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
-                <img src="../src/assets/My_Portfolio_Graphics-removebg-preview.svg" className="h-8" alt="mySiteLogo" />
+                <img src="/assets/logo.svg" className="h-8" alt="logo" />
                 <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">My Site</span>
             </a>
             <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">

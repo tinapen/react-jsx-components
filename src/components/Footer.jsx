25f0b06e3e3528ef422cd10924ebbdab2a0e78c3
Tinapen <cristinapenaflor20@gmail.com>
2025-08-06 23:15:05 +0800

    react jsx components

---
+++ src/components/Footer.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+    
+
+<footer className="fixed bottom-0 left-0 z-20 w-full bg-white shadow-sm dark:bg-gray-900">
+    <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+        <div className="sm:flex sm:items-center sm:justify-between">
+            <a href="https://flowbite.com/" className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
+                <img src="../src/assets/My_Portfolio_Graphics-removebg-preview.svg" className="h-8" alt="mySiteLogo" />
+                <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">My Site</span>
+            </a>
+            <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
+                <li>
+                    <a href="#" className="hover:underline me-4 md:me-6">About</a>
+                </li>
+                <li>
+                    <a href="#" className="hover:underline me-4 md:me-6">Privacy Policy</a>
+                </li>
+                <li>
+                    <a href="#" className="hover:underline me-4 md:me-6">Licensing</a>
+                </li>
+                <li>
+                    <a href="#" className="hover:underline">Contact</a>
+                </li>
+            </ul>
+        </div>
+        <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+        <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2025 <a href="https://flowbite.com/" className="hover:underline">Bheema Infotech | Internship</a>. All Rights Reserved.</span>
+    </div>
+</footer>
+
+
+    </>
+  )
+}
+
+export default Footer
